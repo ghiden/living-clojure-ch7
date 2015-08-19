daae64473fc5cb359b251918e9a04a51ec246da6
Hidenari Nozaki <hidenari@gmail.com>
2015-08-19 22:28:39 +1200

    Add clojurescript stuff

--- project.clj
+++ project.clj
@@ -3,11 +3,28 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2411"]
+                 [cljs-http "0.1.37"]
                  [compojure "1.3.1"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [ring/ring-defaults "0.1.2"]
-                 [ring/ring-json "0.3.1"]]
-  :plugins [[lein-ring "0.8.13"]]
+                 [ring/ring-json "0.3.1"]
+                 [enfocus "2.1.1"]]
+  :plugins [[lein-ring "0.8.13"]
+            [lein-cljsbuild "1.0.6"]]
   :ring {:handler cheshire-cat.handler/app}
+  :cljsbuild {
+              :builds [
+                       {
+                        :source-paths ["src-cljs"]
+                        :compiler {
+                                   :output-to "resources/public/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true
+                                   }
+                        }
+                       ]
+              }
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
---
+++ src-cljs/core.cljs
@@ -0,0 +1,17 @@
+(ns cheshire-cat.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [clojure.browser.repl :as repl]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]
+            [enfocus.core :as ef]))
+
+(defn ^:export init []
+  (repl/connect "http://localhost:9000/repl")
+  (go
+    (let [response (<! (http/get "/cheshire-cat"))
+          body (:body response)]
+      (.log js/console body (:name body) (:status body))
+      (ef/at "#cat-name" (ef/content (:name body)))
+      (ef/at "#status" (ef/content (:status body))))))
+
+
